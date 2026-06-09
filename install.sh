6e8c69df7f22ff7111e0ec7dff81fa59984a71d5
readerofalltrades <fatintkishan@gmail.com>
2026-06-09 22:16:16 +0600

    removed chance of infinite symlinks by using the install script.

--- install.sh
+++ install.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-
 DOTFILES="$HOME/ReaderOS/dotfiles"
 CONFIG="$HOME/.config"
 LOCAL_BIN="$HOME/.local/bin"
 
-echo ":: Linking configs..."
+mkdir -p "$CONFIG" "$LOCAL_BIN"
 
+echo ":: Linking configs..."
 for dir in "$DOTFILES/.config"/*/; do
     name=$(basename "$dir")
+    rm -rf "$CONFIG/$name"
     ln -sf "${dir%/}" "$CONFIG/$name"
     echo "   linked $name"
 done
@@ -15,6 +16,7 @@ done
 echo ":: Linking scripts..."
 for file in "$DOTFILES/.local/bin"/*; do
     name=$(basename "$file")
+    rm -f "$LOCAL_BIN/$name"
     ln -sf "$file" "$LOCAL_BIN/$name"
     echo "   linked $name"
 done
